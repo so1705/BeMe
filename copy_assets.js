0639e110cb0ead052aa43bb8f8ada660e58d020e
so1705 <GitHub>
2026-03-09 22:09:26 +0900

    massive 100-point overhaul: natural photos and rich data grids

--- copy_assets.js
+++ copy_assets.js
@@ -4,7 +4,7 @@ const path = require('path');
 const srcDir = path.join(__dirname, '../../brain/6900568d-9846-4393-81e3-80495a6a1a3f');
 const destDir = path.join(__dirname, 'assets/images');
 
-fs.copyFileSync(path.join(srcDir, 'premium_events_network_1773060602084.png'), path.join(destDir, 'premium_events_network.png'));
-fs.copyFileSync(path.join(srcDir, 'premium_media_sns_1773060627994.png'), path.join(destDir, 'premium_media_sns.png'));
-fs.copyFileSync(path.join(srcDir, 'premium_internship_execution_1773060748231.png'), path.join(destDir, 'premium_internship_execution.png'));
+fs.copyFileSync(path.join(srcDir, 'natural_events_1773061489305.png'), path.join(destDir, 'natural_events.png'));
+fs.copyFileSync(path.join(srcDir, 'natural_sns_media_1773061523154.png'), path.join(destDir, 'natural_sns_media.png'));
+fs.copyFileSync(path.join(srcDir, 'natural_internship_1773061544699.png'), path.join(destDir, 'natural_internship.png'));
 console.log('Images successfully copied to assets/images/');
